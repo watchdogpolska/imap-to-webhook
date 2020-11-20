38010b6cfd965be50312dd5cbef1e8893a19d951
dzejkob <messer>
2020-11-20 15:36:01 +0100

    Set Content-Type version to v2.

--- daemon.py
+++ daemon.py
@@ -13,7 +13,7 @@ def main():
     config = get_config(os.environ)
     session = requests.Session()
     session.headers = {
-        'Content-Type': 'application/imap-to-webhook-v1+json'
+        'Content-Type': 'application/imap-to-webhook-v2+json'
     }
     print("Configuration: ", config)
     if config['sentry_dsn']:
